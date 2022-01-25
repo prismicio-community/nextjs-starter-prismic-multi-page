6bd479842317816531ed3a52e03f41b60dbc0a8e
Phil Snow <phil.snow@prismic.io>
2022-01-25 15:58:59 +0100

    0.2.0 release

--- pages/slice-simulator.jsx
+++ pages/slice-simulator.jsx
@@ -1,13 +1,14 @@
-// e.g. ~/pages/slice-simulator.jsx
 import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import SliceZone from "next-slicezone";
 
 import state from "../.slicemachine/libraries-state.json";
-import resolver from  "../sm-resolver";
+
+import * as Slices from "../slices";
+const resolver = ({ sliceName }) => Slices[sliceName];
 
 const SliceSimulatorPage = () => (<SliceSimulator
 	// The `sliceZone` prop should be a function receiving slices and rendering them using your `SliceZone` component.
-	sliceZone={({ slices }) => <SliceZone slices={slices} resolver={resolver} />}
+	sliceZone={(props) => <SliceZone {...props} resolver={resolver} />}
 	state={state}
 />);
 
