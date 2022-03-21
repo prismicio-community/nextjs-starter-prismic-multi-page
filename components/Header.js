923076297403e6fa5df9a1e1dbf11a85db5a1cfc
Pau <32859437+pvpg@users.noreply.github.com>
2022-03-20 19:57:54 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,22 @@
-import { PrismicLink, PrismicRichText } from "@prismicio/react"
+import { PrismicLink, PrismicRichText } from "@prismicio/react";
 
-export const Header = ({ menu }) => {
-  return (
-    <header className="relative -mb-px border-b border-neutral-200 bg-white px-6 py-4">
-      <div className="mx-auto flex max-w-5xl items-baseline md:items-center">
-        <PrismicLink href="/" className="relative -m-4 p-4 text-xl font-black">
-          <PrismicRichText field={menu.data?.title} />
-        </PrismicLink>
-        <nav className="flex-grow">
-          <ul className="-ml-5 -mt-5 flex justify-end">
-            {menu.data?.menu_links.map((menuLink) => (
-              <li key={menuLink.link.id} className="pt-5 pl-5">
-                <PrismicLink field={menuLink.link} className="text-neutral-500">
-                  <PrismicRichText field={menuLink.label} />
-                </PrismicLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </header>
-  )
-}
+export const Header = ({ menu }) => (
+  <header className="relative -mb-px border-b border-neutral-200 bg-white px-6 py-4">
+    <div className="mx-auto flex max-w-5xl items-baseline md:items-center">
+      <PrismicLink href="/" className="relative -m-4 p-4 text-xl font-black">
+        <PrismicRichText field={menu.data?.title} />
+      </PrismicLink>
+      <nav className="flex-grow">
+        <ul className="-ml-5 -mt-5 flex justify-end">
+          {menu.data?.menu_links.map((menuLink) => (
+            <li key={menuLink.link.id} className="pt-5 pl-5">
+              <PrismicLink field={menuLink.link} className="text-neutral-500">
+                <PrismicRichText field={menuLink.label} />
+              </PrismicLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
