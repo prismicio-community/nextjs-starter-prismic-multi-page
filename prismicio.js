c69d3f26bcec4f3c6cf4131cbc505dc277436662
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-11-16 14:50:22 -1000

    refactor: move Route Resolver into its own constant

--- prismicio.js
+++ prismicio.js
@@ -9,26 +9,43 @@ import sm from "./sm.json";
 export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
 
 /**
+ * The project's Prismic Route Resolvers. This list determines a Prismic document's URL.
+ *
+ * @type {prismic.ClientConfig['routes']}
+ */
+const routes = [
+  {
+    type: "page",
+    path: "/:uid",
+  },
+  {
+    type: "page",
+    path: "/",
+    uid: "home",
+  },
+  {
+    type: "settings",
+    path: "/",
+  },
+  {
+    type: "navigation",
+    path: "/",
+  },
+];
+
+/**
  * Creates a Prismic client for the project's repository. The client is used to
  * query content from the Prismic API.
  *
  * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
  */
-export const createClient = (config = {}) => {
+export const createClient = ({ previewData, req, ...config } = {}) => {
   const client = prismic.createClient(sm.apiEndpoint, {
-    routes: [
-      { type: "page", path: "/:uid" },
-      { type: "page", path: "/", uid: "home" },
-      { type: "settings", path: "/" },
-      { type: "navigation", path: "/" },
-    ],
+    routes,
+    ...config,
   });
 
-  prismicNext.enableAutoPreviews({
-    client,
-    previewData: config.previewData,
-    req: config.req,
-  });
+  prismicNext.enableAutoPreviews({ client, previewData, req });
 
   return client;
 };
