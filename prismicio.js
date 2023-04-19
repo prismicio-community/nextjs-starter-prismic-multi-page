7f64ed3e74bfc4cd5837798528a7607973a0a3ec
Hugo Villain <hugo.p.villain@gmail.com>
2023-04-19 12:06:35 +0200

    plugins

--- prismicio.js
+++ prismicio.js
@@ -1,12 +1,12 @@
 import * as prismic from "@prismicio/client";
 import * as prismicNext from "@prismicio/next";
 
-import sm from "./sm.json";
+import sm from "./slicemachine.config.json";
 
 /**
  * The project's Prismic repository name.
  */
-export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
+const { repositoryName } = sm;
 
 /**
  * The project's Prismic Route Resolvers. This list determines a Prismic document's URL.
@@ -40,7 +40,7 @@ const routes = [
  * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
  */
 export const createClient = ({ previewData, req, ...config } = {}) => {
-  const client = prismic.createClient(sm.apiEndpoint, {
+  const client = prismic.createClient(repositoryName, {
     routes,
     ...config,
   });
@@ -49,3 +49,5 @@ export const createClient = ({ previewData, req, ...config } = {}) => {
 
   return client;
 };
+
+export { repositoryName };
