06a0997e3286f66d8be1641284193b3672e68000
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2025-10-22 11:32:23 -1000

    feat: support Next.js 16 (#47)
    * feat: support Next.js 16
    
    * fix: update slice machine packages
    
    * fix: add "max" cacheLife

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
 export async function POST() {
-  revalidateTag("prismic");
+  revalidateTag("prismic", "max");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
